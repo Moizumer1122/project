baa537d41025e445b878ac66680009d5baeea232
umerbandesha <umerbandesha007@gmail.com>
2025-03-23 07:02:04 +0500

    contact page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,13 +1,16 @@
+import green from '../../public/images/green.png';
+
 const ContactUs = () => {
     return ( 
     <div style={{backgroundColor:'#121B25'}}>
         
-    <h1 style={{paddingLeft:'290px',paddingTop:'50px'}}>Let’s Simply<br/> begin with <br/>Rentiz.</h1> 
+    <h1 style={{paddingLeft:'290px',paddingTop:'50px',fontSize:'50px'}}>Let’s Simply<br/> begin with <br/>Rentiz.</h1> 
         
-        
-            <div class="dropdown">
+        <p style={{paddingLeft:'290px',paddingTop:'50px',fontSize:'15px'}}>Neque, vestibulum sed varius magna et at. Eu, adipiscing morbi augue <br/>justo. Nibh laoreet volutpat quis velit. Blandit aliquam donec sed morbi <br/>congue eget lorem viverra porta id lobortis.</p>
+            <div class="dropdown" style={{paddingLeft:'290px',paddingTop:'50px',fontSize:'15px'}}>
            <button class="button">Get Started</button>
        </div>
+       <section><img src={green} alt=""  /></section>
         
         </div>
 
